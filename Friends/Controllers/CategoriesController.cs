d50d551fbf81438a8826dadfcc41433c0926149d
Mohammed <mezdn@outlook.com>
2020-04-22 16:48:27 +0300

    Add Post and Category Controllers

---
+++ Friends/Controllers/CategoriesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Friends.Models;
+
+namespace Friends.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            // TODO #8: 
+            // <query> Select all Categories </query>
+            // <output> List of Categories </output>
+
+            return View();
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = new Category();
+            // TODO #9: 
+            // <query> Select all properties of a Category of ID `id` </query>
+            // <input> id </input>
+            // <output> Category </output>
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,AreaOfExpertiseName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO #10: 
+                // <query> Create a new Category given its properties </query>
+                // <input> Category(ID, Name, AreaOfExpertiseName) </input>
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = new Category();
+            // TODO #11: Duplicate of TODO #9 
+
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,AreaOfExpertiseName")] Category category)
+        {
+            if (id != category.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // TODO #12: 
+                // <query> Edit an old Category given its id and the ne values of its properties </query>
+                // <input> id, Category(Name, AreaOfExpertiseName) </input>
+                // <input> Category </input>
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // TODO #13: Duplicate of TODO #9 
+
+            var category = new Category();
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            // TODO #14: 
+            // <query> Delete a category of ID `id`</query>
+            // <input> id</input>
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Friends/Controllers/PostController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Friends.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Friends.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index(int userID)
+        {
+            // TODO #1: 
+            // <query> Select all posts owned by person with ID `userID` </query>
+            // <input> id, userID </input>
+            // <output> List of Posts </output>
+
+            return View();
+        }
+
+        // GET: Post/Details/5
+        public ActionResult Details(int id)
+        {
+            // TODO #2: 
+            // <query> Select all properties for a post of ID `id` </query>
+            // <input> id </input>
+            // <output> Post </output>
+            return View();
+        }
+
+        // GET: Post/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Post/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(int userID, [Bind("ID,TimeOfCreation,Content,GroupID")] Post post)
+        {
+            try
+            {
+                // TODO #3: 
+                // <query> Create a new post given its properties </query>
+                // <input> post(ID, TimeOfCreation, Content, userID, GroupID) </input>
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Edit/5
+        public ActionResult Edit(int id)
+        {
+            // TODO #4: Duplicate of TODO #2 
+            return View();
+        }
+
+        // POST: Post/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, int userID, [Bind("ID,TimeOfCreation,Content,GroupID")] Post post)
+        {
+            try
+            {
+                // TODO #5: 
+                // <query> Edit an old post given its id and the new vlaues of its properties IF it is owned by a user of ID userID</query>
+                // <input> post(ID, TimeOfCreation, Content, GroupID), userID </input>
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Delete/5
+        public ActionResult Delete(int id, int userID)
+        {
+            // TODO #6: 
+            // <query> Get a post of ID `id` if its owner has id `userID` </query>
+            // <input> id, userID </input>
+            // <output> post </output>
+
+            return View();
+        }
+
+        // POST: Post/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                // TODO #7: 
+                // <query> Delete a post of ID `id`</query>
+                // <input> id</input>
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
